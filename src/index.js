64bf6049da74e8f7cec91bdb5406d3459144a5da
Gordon Doskas <gordon2012@users.noreply.github.com>
2017-02-04 08:54:02 -0800

    Add basic structure WIP

--- src/index.js
+++ src/index.js
@@ -1,4 +1,15 @@
 require('file-loader?name=[name].[ext]!./index.html');
 require('./style.css');
 
-document.querySelector('button').addEventListener('click', e => console.log('World'));
+const content = document.querySelector('.content');
+
+function resize() {
+    const dia = Math.min(window.innerWidth, window.innerHeight);
+    const margin = (window.innerHeight - dia) / 2;
+    
+    const padding = 32;
+    content.setAttribute('style', `width: ${dia-padding*2}px; height: ${dia-padding*2}px; margin-top: ${margin}px; padding: 32px`);
+}
+resize();
+
+window.onresize = resize;
